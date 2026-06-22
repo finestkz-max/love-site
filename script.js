1f5cd5ad32cfb992a6dfdce73a1026d6397bc68a
finestkz-max <finest.kz@gmail.com>
2026-06-23 01:30:39 +0500

    Initial site

---
+++ script.js
@@ -0,0 +1,71 @@
+
+const music = document.getElementById("music");
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const surpriseBtn = document.getElementById("surpriseBtn");
+
+const mainScreen = document.getElementById("main-screen");
+const movieScreen = document.getElementById("movie-screen");
+const credits = document.getElementById("credits");
+
+// Дни вместе
+
+
+// Запуск музыки после первого касания
+
+document.body.addEventListener(
+    "click",
+    () => {
+        music.play();
+    },
+    { once: true }
+);
+
+// Да ❤️
+
+yesBtn.addEventListener("click", () => {
+
+    alert("❤️ Я знала ❤️\n\nОбожаю тебя очень сильно 🐱💕");
+
+});
+
+// Нет 😈
+
+noBtn.style.left = "60%";
+noBtn.style.top = "75%";
+
+noBtn.addEventListener("touchstart", moveNo);
+noBtn.addEventListener("mouseover", moveNo);
+
+function moveNo() {
+
+    const x = Math.random() * 70;
+    const y = Math.random() * 70;
+
+    noBtn.style.left = x + "%";
+    noBtn.style.top = y + "%";
+}
+
+// Нажми сюда ❤️
+
+surpriseBtn.addEventListener("click", () => {
+
+    mainScreen.style.display = "none";
+    movieScreen.style.display = "block";
+
+    let position = 1200;
+
+    credits.style.bottom = -position + "px";
+
+    function scrollCredits() {
+
+        position -= 1;
+
+        credits.style.bottom = (-position) + "px";
+
+        requestAnimationFrame(scrollCredits);
+    }
+
+    scrollCredits();
+
+});
